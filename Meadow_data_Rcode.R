ca20def3df0f401cbd06014e868b440b6954d41c
Darwin Sodhi <darwin193@live.com>
2018-09-28 15:45:46 -0700

    Plots
    I added some plots

--- Meadow_data_Rcode.R
+++ Meadow_data_Rcode.R
@@ -3,3 +3,6 @@ setwd("~/Documents/github/Sandbox") # Deirdre's working directory
 mdw<-read.csv("~/Documents/github/Sandbox/MeadowTraitsDSV.csv")
 
 head(mdw)
+
+plot(Height..M.~DSV, data=mdw)
+plot(Name~Height..M., data = mdw)
